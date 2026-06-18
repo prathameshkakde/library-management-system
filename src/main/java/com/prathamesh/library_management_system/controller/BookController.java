4fc52f4aaa131fe4892e2a311ed0a8a922df929a
Prathamesh Kakde <kakdeprathamesh860@gmail.com>
2026-06-19 02:06:24 +0530

    feat: display books in thymeleaf table

--- src/main/java/com/prathamesh/library_management_system/controller/BookController.java
+++ src/main/java/com/prathamesh/library_management_system/controller/BookController.java
@@ -1,7 +1,10 @@
 package com.prathamesh.library_management_system.controller;
 
+import com.prathamesh.library_management_system.service.BookService;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 
 /**
@@ -10,8 +13,21 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class BookController {
 
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/books")
+    public String books(Model model) {
+
+        model.addAttribute("books", bookService.getAllBooks());
+        return "books";
     }
 }
