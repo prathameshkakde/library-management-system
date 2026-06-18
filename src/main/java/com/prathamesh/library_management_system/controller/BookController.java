cc570c6b23f14e3f241e0d2c828969600f7d94b9
Prathamesh Kakde <kakdeprathamesh860@gmail.com>
2026-06-18 22:30:28 +0530

    feat: add thymeleaf home page

--- src/main/java/com/prathamesh/library_management_system/controller/BookController.java
+++ src/main/java/com/prathamesh/library_management_system/controller/BookController.java
@@ -1,7 +1,6 @@
 package com.prathamesh.library_management_system.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.stereotype.Controller;
 
 
@@ -12,8 +11,7 @@ import org.springframework.stereotype.Controller;
 public class BookController {
 
     @GetMapping("/")
-    @ResponseBody
     public String home() {
-        return "Welcome to Library Management System";
+        return "home";
     }
 }
