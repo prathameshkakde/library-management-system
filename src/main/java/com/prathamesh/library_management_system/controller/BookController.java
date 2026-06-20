f9125d653d90b5ff586663b731f58e5aa69405b6
Prathamesh Kakde <kakdeprathamesh860@gmail.com>
2026-06-21 02:20:15 +0530

    feat: implement add book form and save functionality

--- src/main/java/com/prathamesh/library_management_system/controller/BookController.java
+++ src/main/java/com/prathamesh/library_management_system/controller/BookController.java
@@ -1,10 +1,13 @@
 package com.prathamesh.library_management_system.controller;
 
+import com.prathamesh.library_management_system.entity.Book;
 import com.prathamesh.library_management_system.service.BookService;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 
 /**
@@ -29,5 +32,18 @@ public class BookController {
 
         model.addAttribute("books", bookService.getAllBooks());
         return "books";
+    }
+
+    @GetMapping("/books/add")
+    public String showAddBookForm() {
+        return "add-book";
+    }
+
+    @PostMapping("/books/save")
+    public String saveBook(
+            @ModelAttribute Book book) {
+
+        bookService.saveBook(book);
+        return "redirect:/books";
     }
 }
